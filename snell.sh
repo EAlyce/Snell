b675184c33c3da09087b66e80646ea8dc3fe1f3e
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-06-06 13:13:48 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,49 +1,33 @@
-#!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-CONF="/etc/snell/snell-server.conf"
-SYSTEMD="/etc/systemd/system/snell.service"
-apt-get install unzip -y
-cd ~/
-wget --no-check-certificate -O snell.zip https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-aarch64.zip
-unzip -o snell.zip
- rm -f snell.zip
- chmod +x snell-server
- mv -f snell-server /usr/local/bin/
- if [ -f ${CONF} ]; then
-   echo "Found existing config..."
-   else
-   if [ -z ${PSK} ]; then
-     PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
-     echo "Using generated PSK: ${PSK}"
-   else
-     echo "Using predefined PSK: ${PSK}"
-   fi
-   mkdir /etc/snell/
-   echo "Generating new config..."
-   echo "[snell-server]" >>${CONF}
-   echo "listen = 0.0.0.0:13254" >>${CONF}
-   echo "psk = ${PSK}" >>${CONF}
-   echo "obfs = tls" >>${CONF}
- fi
- if [ -f ${SYSTEMD} ]; then
-   echo "Found existing service..."
-   systemctl daemon-reload
-   systemctl restart snell
- else
-   echo "Generating new service..."
-   echo "[Unit]" >>${SYSTEMD}
-   echo "Description=Snell Proxy Service" >>${SYSTEMD}
-   echo "After=network.target" >>${SYSTEMD}
-   echo "" >>${SYSTEMD}
-   echo "[Service]" >>${SYSTEMD}
-   echo "Type=simple" >>${SYSTEMD}
-   echo "LimitNOFILE=32768" >>${SYSTEMD}
-   echo "ExecStart=/usr/local/bin/snell-server -c /etc/snell/snell-server.conf" >>${SYSTEMD}
-   echo "" >>${SYSTEMD}
-   echo "[Install]" >>${SYSTEMD}
-   echo "WantedBy=multi-user.target" >>${SYSTEMD}
-   systemctl daemon-reload
-   systemctl enable snell
-   systemctl start snell
- fi
+#!/bin/bash
+
+if cat /etc/*-release | grep -q -E -i "debian|ubuntu|armbian|deepin|mint"; then 	# install dependencies
+	apt-get install wget unzip dpkg -y
+elif cat /etc/*-release | grep -q -E -i "centos|red hat|redhat"; then
+	yum install wget unzip dpkg -y
+elif cat /etc/*-release | grep -q -E -i "arch|manjora"; then
+	yes | pacman -S wget dpkg unzip
+elif cat /etc/*-release | grep -q -E -i "fedora"; then
+	dnf install wget unzip dpkg -y
+fi
+
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf	# enable bbr
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
+
+cd
+ARCHITECTURE=$(dpkg --print-architecture)
+if [ "$ARCHITECTURE" = "arm64" ]; then
+	ARCHITECTURE="aarch64"
+fi
+wget -c https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-$ARCHITECTURE.zip	# download binary
+unzip -o snell-server-v4.0.1-linux-$ARCHITECTURE.zip
+
+echo -e "[Unit]\nDescription=snell server\n[Service]\nUser=root\nWorkingDirectory=/root\nExecStart=/root/snell-server\nRestart=always\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/snell.service
+y | ./snell-server
+systemctl start snell
+systemctl enable snell			# start service
+
+echo
+echo "Copy the following line to surge"			# print profile
+echo "$(curl -s ipinfo.io/city) = snell, $(curl -s ipinfo.io/ip), $(cat snell-server.conf | grep -i listen | cut --delimiter=':' -f2),psk=$(grep 'psk' snell-server.conf | cut -d= -f2 | tr -d ' '), version=4, tfo=true"
