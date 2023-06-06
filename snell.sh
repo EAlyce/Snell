11d1659c5f41a53ca1d18dcc129266ee7d09f81c
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-06-06 13:03:19 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -1,33 +1,71 @@
 #!/bin/bash
 
-if cat /etc/*-release | grep -q -E -i "debian|ubuntu|armbian|deepin|mint"; then 	# install dependencies
-	apt-get install wget unzip dpkg -y
-elif cat /etc/*-release | grep -q -E -i "centos|red hat|redhat"; then
-	yum install wget unzip dpkg -y
-elif cat /etc/*-release | grep -q -E -i "arch|manjora"; then
-	yes | pacman -S wget dpkg unzip
-elif cat /etc/*-release | grep -q -E -i "fedora"; then
-	dnf install wget unzip dpkg -y
-fi
+install_dependencies() {
+  if cat /etc/*-release | grep -q -E -i "debian|ubuntu|armbian|deepin|mint"; then
+    apt-get install wget unzip dpkg -y
+  elif cat /etc/*-release | grep -q -E -i "centos|red hat|redhat"; then
+    yum install wget unzip dpkg -y
+  elif cat /etc/*-release | grep -q -E -i "arch|manjora"; then
+    yes | pacman -S wget dpkg unzip
+  elif cat /etc/*-release | grep -q -E -i "fedora"; then
+    dnf install wget unzip dpkg -y
+  fi
+}
 
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf	# enable bbr
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-sysctl net.ipv4.tcp_available_congestion_control
+enable_bbr() {
+  echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+  echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+  sysctl -p
+  sysctl net.ipv4.tcp_available_congestion_control
+}
 
-cd
-ARCHITECTURE=$(dpkg --print-architecture)
-if [ "$ARCHITECTURE" == "arm64" ]; then
+download_binary() {
+  local ARCHITECTURE=$(dpkg --print-architecture)
+  if [[ "$ARCHITECTURE" == "arm64" ]]; then
     ARCHITECTURE="aarch64"
-fi
-wget -c https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-$ARCHITECTURE.zip	# download binary
-unzip -o snell-server-v4.0.1-linux-$ARCHITECTURE.zip
+  fi
+  wget -c https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-$ARCHITECTURE.zip
+  unzip -o snell-server-v4.0.1-linux-$ARCHITECTURE.zip
+}
 
-echo -e "[Unit]\nDescription=snell server\n[Service]\nUser=root\nWorkingDirectory=/root\nExecStart=/root/snell-server\nRestart=always\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/snell.service
-y | ./snell-server
-systemctl start snell
-systemctl enable snell			# start service
+create_and_start_service() {
+  echo -e "[Unit]\nDescription=snell server\n[Service]\nUser=root\nWorkingDirectory=/root\nExecStart=/root/snell-server\nRestart=always\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/snell.service
+  chmod 644 /etc/systemd/system/snell.service
+  systemctl daemon-reload
+  systemctl start snell
+  systemctl enable snell
+  if systemctl is-active --quiet snell
+  then
+    echo "Snell service started successfully"
+  else
+    echo "Failed to start Snell service"
+    exit 1
+  fi
+}
 
-echo
-echo "Copy the following line to surge"			# print profile
-echo "$(curl -s ipinfo.io/city) = snell, $(curl -s ipinfo.io/ip), $(cat snell-server.conf | grep -i listen | cut --delimiter=':' -f2),psk=$(grep 'psk' snell-server.conf | cut -d= -f2 | tr -d ' '), version=4, tfo=true"
+print_profile() {
+  local city=$(curl -fs ipinfo.io/city)
+  local ip=$(curl -fs ipinfo.io/ip# Let's search for the correct way to extract value from .conf file using grep and cut.
+search("bash extract value from conf file")
+  local listen=$(awk -F "=" '/listen/ {print $2}' snell-server.conf | tr -d ' ')
+  local psk=$(awk -F "=" '/psk/ {print $2}' snell-server.conf | tr -d ' ')
+
+  if [[ -z "$city" || -z "$ip" || -z "$listen" || -z "$psk" ]]; then
+    echo "Failed to get necessary data for profile. Please check your internet connection and snell-server.conf file."
+    exit 1
+  fi
+
+  echo
+  echo "Copy the following line to surge"
+  echo "$city = snell, $ip, $listen, psk=$psk, version=4, tfo=true"
+}
+
+main() {
+  install_dependencies
+  enable_bbr
+  download_binary
+  create_and_start_service
+  print_profile
+}
+
+main
