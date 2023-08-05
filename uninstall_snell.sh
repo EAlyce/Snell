96aa24f6679b5dede5463a6e7a8cc89152c4cd58
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-08-05 14:22:36 +0800

    更新 uninstall_snell.sh

--- uninstall_snell.sh
+++ uninstall_snell.sh
@@ -18,24 +18,17 @@ for container in $CONTAINERS; do
   container_map[$i]=$id
   i=$((i+1))
 done
-echo "$i. 删除所有容器"
-echo "$((i+1)). 退出脚本"
 
 read -p "输入选择（输入数字）： " choice
 
-if [[ $choice -eq $i ]]; then
-  # 删除所有 Snell 容器
-  docker rm -f $(docker ps -a -q --filter name=Snell)
-  echo "所有 Snell 容器已删除。"
-elif [[ $choice -eq $((i+1)) ]]; then
-  # 退出脚本
-  echo "退出脚本。"
-else
-  # 删除选定的 Snell 容器
-  selected_container=${container_map[$choice]}
+# 删除选定的 Snell 容器
+selected_container=${container_map[$choice]}
+if [ -n "$selected_container" ]; then
   echo "正在停止容器 $selected_container ..."
   docker stop $selected_container
   echo "正在删除容器 $selected_container ..."
   docker rm $selected_container
   echo "容器 $selected_container 已删除。"
+else
+  echo "选择无效."
 fi
