87c94f5d426bb25df0268c7fe444c95a35d1fb38
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-08-05 13:08:22 +0800

    Update install_snell.sh

--- install_snell.sh
+++ install_snell.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-# 作者@ALice
+# 作者 Chat GPT
+# 自用脚本
+
+echo "请选择 Snell 的版本："
+echo "1. v3"
+echo "2. v4"
+read -p "输入选择（1/2）: " choice
+
+# 检测系统类型
+ARCH=$(uname -m)
+if [ "$ARCH" == "x86_64" ]; then
+  ARCH_SUFFIX="amd64.zip"
+elif [ "$ARCH" == "aarch64" ]; then
+  ARCH_SUFFIX="aarch64.zip"
+else
+  echo "不支持的系统架构: $ARCH"
+  exit 1
+fi
+
+# 根据选择设置软件源
+case $choice in
+  1) SNELL_URL="https://github.com/xOS/Others/raw/master/snell/v3.0.1/snell-server-v3.0.1-linux-$ARCH_SUFFIX";;
+  2) SNELL_URL="https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-$ARCH_SUFFIX";;
+  *) echo "无效选择"; exit 1;;
+esac
+
 # 安装所需依赖
 apt-get install -y wget unzip docker-compose
 
@@ -43,12 +68,16 @@ docker-compose pull && docker-compose up -d
 
 # 打印节点内容
 echo
-echo "- name: Snell$PORT_NUMBER"
-echo "  type: snell"
-echo "  server: $(curl -s ifconfig.me)"
-echo "  port: $PORT_NUMBER"
-echo "  psk: $PASSWORD"
-echo "  version: $VERSION_NUMBER"
-echo "  udp: true"
-
-echo "Snell$PORT_NUMBER = snell, $(curl -s ifconfig.me), $PORT_NUMBER, psk=$PASSWORD, version=$VERSION_NUMBER, tfo=true"
+if [ "$choice" == "1" ]; then
+  echo "- name: Snell$PORT_NUMBER"
+  echo "  type: snell"
+  echo "  server: $(curl -s ifconfig.me)"
+  echo "  port: $PORT_NUMBER"
+  echo "  psk: $PASSWORD"
+  echo "  version: $VERSION_NUMBER"
+  echo "  udp: true"
+  echo
+  echo "Snell$PORT_NUMBER = snell, $(curl -s ifconfig.me), $PORT_NUMBER, psk=$PASSWORD, version=$VERSION_NUMBER, tfo=true"
+elif [ "$choice" == "2" ]; then
+  echo "Snell$PORT_NUMBER = snell, $(curl -s ifconfig.me), $PORT_NUMBER, psk=$PASSWORD, version=$VERSION_NUMBER, tfo=true"
+fi
