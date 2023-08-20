22a94767625285c808135a2d9e2cf7881e1ebc88
ExaAlice <48439969+ExaAlice@users.noreply.github.com>
2023-08-20 18:57:39 +0800

    Update

--- uninstall_snell.sh
+++ uninstall_snell.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-# 删除选定的容器
+# 删除选定的容器和相关持久化文件夹
 function remove_container() {
   selected_container=$1
   container_name=$(docker ps --filter "id=$selected_container" --format "{{.Names}}")
+  mounted_folders=$(docker inspect --format '{{ range .Mounts }}{{ .Source }} {{ end }}' $selected_container)
 
   if [ -n "$selected_container" ]; then
     echo "正在停止容器 $selected_container ..."
     docker stop $selected_container
-    if [ $? -ne 0 ]; then
-      echo "停止容器失败。"
-      return
-    fi
-    echo "正在查找与容器名 $container_name 相同的文件夹..."
-    find / -type d -name "$container_name" -exec echo "找到文件夹 '{}' " \;
-    read -p "确定要删除容器及找到的文件夹吗? (y/N): " confirmation
-    if [ "$confirmation" != "y" ]; then
-      echo "删除操作已取消。"
-      return
-    fi
     echo "正在删除容器 $selected_container ..."
     docker rm $selected_container
-    if [ $? -ne 0 ]; then
-      echo "删除容器失败。"
-      return
-    fi
     echo "容器 $selected_container 已删除。"
-    find / -type d -name "$container_name" -exec sh -c 'echo "正在删除文件夹 '\''{}'\'' ..."; rm -rf "{}"; echo "文件夹 '\''{}'\'' 已删除。"' \;
+    for folder in $mounted_folders; do
+      echo "正在删除文件夹 '$folder' ..."
+      rm -rf "$folder"
+      echo "文件夹 '$folder' 已删除。"
+    done
   else
     echo "未知错误，无法找到容器。"
   fi
@@ -59,7 +49,6 @@ function list_containers() {
     container_map[$i]="exit"
     read -p "输入选择（输入数字）： " choice
 
-    # 检查用户输入是否为数字且在范围内
     if [[ $choice =~ ^[0-9]+$ ]] && [ $choice -ge 1 ] && [ $choice -le $i ]; then
       if [ "${container_map[$choice]}" == "all" ]; then
         for key in ${!container_map[@]}; do
@@ -79,4 +68,3 @@ function list_containers() {
 }
 
 list_containers
-
