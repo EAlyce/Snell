4512b114a6640396e9ad3deb44577db4bd6cfa0c
Els <48439969+EAlyce@users.noreply.github.com>
2023-12-16 17:58:44 +0800

    Update install_snell.sh

--- install_snell.sh
+++ install_snell.sh
@@ -1,49 +1,27 @@
 #!/bin/bash
 
 # 验证当前用户是否为root。
-if [ "$(id -u)" != "0" ]; then
-    echo "Error: You must be root to run this script"
-    exit 1
-fi
+[ "$(id -u)" != "0" ] && echo "Error: You must be root to run this script" && exit 1
 
 sudo apt-get install -y curl wget
 
 # 检测是否已安装Docker
-if ! command -v docker &> /dev/null; then
-    # 未安装，执行安装命令
-    curl -fsSL https://test.docker.com | bash
-else
-    # 已安装，输出提示信息
-    echo "Docker已安装"
-fi
+command -v docker &> /dev/null || { echo "Installing Docker..."; curl -fsSL https://test.docker.com | bash; }
+command -v docker &> /dev/null && echo "Docker已安装"
 
 # 检测是否已安装Docker Compose
-if ! command -v docker-compose &> /dev/null; then
-    # 未安装，执行安装命令
-    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-
-    # 检查安装是否成功
-    if ! command -v docker-compose &> /dev/null; then
-        echo "Docker Compose安装失败。"
-        exit 1
-    fi
-else
-    # 已安装，输出提示信息
-    echo "Docker Compose已安装。"
-fi
+command -v docker-compose &> /dev/null || { echo "Installing Docker Compose..."; sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose; }
+command -v docker-compose &> /dev/null && echo "Docker Compose已安装" || { echo "Docker Compose安装失败。"; exit 1; }
 
 # 输出Docker Compose的版本信息
 docker-compose --version
+docker --version
 
 # 定义公网IP获取服务列表
 ip_services=("ifconfig.me" "ipinfo.io/ip" "icanhazip.com" "ipecho.net/plain" "ident.me")
 
 # 检查网络连接
-if ! ping -c 1 8.8.8.8 &> /dev/null; then
-    echo "网络连接不可用。"
-    exit 1
-fi
+ping -c 1 8.8.8.8 &> /dev/null || { echo "网络连接不可用。"; exit 1; }
 
 # 循环尝试获取公网IP
 public_ip=""
@@ -59,36 +37,26 @@ for service in "${ip_services[@]}"; do
 done
 
 # 检查是否成功获取公网IP
-if [[ ! "$public_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "所有服务都无法获取公网IP。"
-    exit 1
-fi
+[[ "$public_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] || { echo "所有服务都无法获取公网IP。"; exit 1; }
 
 # 检查网络连接
-if ! ping -c 1 8.8.8.8 >/dev/null 2>&1; then
-    echo "网络连接不正常。无法获取城市名。"
-    exit 1
-fi
+ping -c 1 8.8.8.8 >/dev/null 2>&1 || { echo "网络连接不正常。无法获取城市名。"; exit 1; }
 
-# 尝试使用ip-api.com获取城市名
-LOCATION=$(curl -s "http://ip-api.com/line?fields=city")
+# 定义获取城市名的服务列表
+location_services=("http://ip-api.com/line?fields=city" "ipinfo.io/city" "https://ip-api.io/json | jq -r .city")
 
-# 如果失败，尝试使用ipinfo.io获取城市名
-if [ -z "$LOCATION" ]; then
-    LOCATION=$(curl -s ipinfo.io/city)
-fi
+# 循环尝试获取城市名
+LOCATION=""
+for service in "${location_services[@]}"; do
+    LOCATION=$(curl -s "$service")
+    if [ -n "$LOCATION" ]; then
+        echo "主机位置：$LOCATION"
+        break
+    fi
+done
 
-# 如果还是失败，尝试使用ip-api.io获取城市名
-if [ -z "$LOCATION" ]; then
-    LOCATION=$(curl -s "https://ip-api.io/json" | jq -r .city)
-fi
-
-# 如果所有服务都失败，输出错误消息
-if [ -z "$LOCATION" ]; then
-    echo "无法获取城市名。"
-else
-    echo "主机位置：$LOCATION"
-fi
+# 检查是否成功获取城市名
+[ -n "$LOCATION" ] || echo "无法获取城市名。"
 
 echo -e "nameserver 8.8.4.4\nnameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 
@@ -97,29 +65,23 @@ sudo pkill -9 apt || true
 sudo pkill -9 dpkg || true
 
 # 检查锁文件是否存在，如果存在则移除它们
-if [ -f "/var/lib/dpkg/lock-frontend" ]; then
-    sudo rm -f /var/lib/dpkg/lock-frontend
-fi
-if [ -f "/var/lib/apt/lists/lock" ]; then
-    sudo rm -f /var/lib/apt/lists/lock
-fi
+sudo rm -f /var/lib/dpkg/lock-frontend /var/lib/apt/lists/lock
 
 # 配置未配置的包
 sudo dpkg --configure -a
 
-# 更新包列表
-sudo apt-get update -y
+# 更新包列表并安装软件包
+sudo apt-get update -y && sudo apt-get install -y curl wget git vim nano sudo iptables python3 python3-pip net-tools unzip zip gcc g++ make jq netcat-traditional iptables-persistent
 
-# 安装软件包
-sudo apt-get install -y curl wget git vim nano sudo iptables python3 python3-pip net-tools unzip zip gcc g++ make jq netcat-traditional iptables-persistent
+# 配置pip
 echo -e "[global]\nbreak-system-packages = true" | sudo tee /etc/pip.conf
-# 更新包和依赖
-sudo apt-get upgrade -y
-pip3 list --outdated | awk 'NR>2 {print $1}' | xargs -n1 pip3 install -U
-sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y && sudo apt-get install only-upgrade python3 -y
 
-# 清理垃圾
-sudo apt autoremove -y
+# 更新包和依赖
+sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
+pip3 list --outdated | awk 'NR>2 {print $1}' | xargs -n1 pip3 install -U
+
+# 清理垃圾并更新python3
+sudo apt-get autoremove -y && sudo apt-get install only-upgrade python3 -y
 
 clear
 
@@ -142,13 +104,13 @@ if [ -f "/etc/debian_version" ]; then
     echo "apt command not found"
   fi
 fi
+
 # 检查/proc/sys/net/ipv4/tcp_fastopen文件是否存在，如果存在则启用TFO客户端功能
-if [ -f "/proc/sys/net/ipv4/tcp_fastopen" ]; then
-    echo 3 | sudo tee /proc/sys/net/ipv4/tcp_fastopen
-fi
+[ -f "/proc/sys/net/ipv4/tcp_fastopen" ] && echo 3 | sudo tee /proc/sys/net/ipv4/tcp_fastopen
 
 docker system prune -af --volumes
 echo "清理完成"
+
 # 如果您使用的是iptables，允许TFO数据包
 sudo iptables -A INPUT -p tcp --tcp-flags SYN SYN -j ACCEPT
 
