9982cb3800c9c28e97b9e0d6abdd3d1ea897a11d
Els <48439969+EAlyce@users.noreply.github.com>
2023-12-27 23:54:49 +0800

    Update install_snell.sh

--- install_snell.sh
+++ install_snell.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# 定义设置 PATH 的函数
 set_custom_path() {
     # 检查是否存在 PATH 变量，如果不存在则设置
     PATH_CHECK=$(crontab -l | grep -q '^PATH=' && echo "true" || echo "false")
@@ -10,41 +9,64 @@ set_custom_path() {
     fi
 }
 
-# 调用设置 PATH 函数
-set_custom_path
 check_root() {
     [ "$(id -u)" != "0" ] && echo "Error: You must be root to run this script" && exit 1
 }
 
 install_tools() {
-    sudo apt-get install -y curl wget || true
+    # 隐藏安装工具函数的输出
+    sudo apt-get install -y curl wget > /dev/null || true
+    sudo apt-get install tmux mosh > /dev/null
 }
 
 clean_lock_files() {
-    sudo pkill -9 apt || true
-    sudo pkill -9 dpkg || true
+    # 隐藏清理锁文件和终止进程函数的输出
+    sudo pkill -9 apt > /dev/null || true
+    sudo pkill -9 dpkg > /dev/null || true
     sudo rm -f /var/lib/dpkg/lock-frontend /var/lib/apt/lists/lock
-    sudo dpkg --configure -a
+    sudo dpkg --configure -a > /dev/null
 }
 
+# 错误代码
+ERR_DOCKER_INSTALL=1
+ERR_COMPOSE_INSTALL=2
+
+
+# 安装Docker和Docker Compose
 install_docker_and_compose() {
+    # 为 tmux会话创建一个唯一名字
+    SESSION_NAME="docker_install_$(date +%s)"
+
+    # 创建新的tmux会话
+    tmux new-session -d -s "$SESSION_NAME"
+
+    # 向 tmux会话发送要执行的命令
+    tmux send-keys "
+
+    # 在bash上设置错误处理
+    set -e
+
+    # 现在，在这个新创建的tmux会话中执行安装过程
+    echo \"开始安装...\"
+
     # 安装Docker
-    command -v docker &> /dev/null || {
-        echo "Installing Docker...";
-        curl -fsSL https://get.docker.com | bash
-    }
-    command -v docker &> /dev/null && echo "Docker已安装"
+    if ! command -v docker &> /dev/null; then
+        curl -fsSL https://get.docker.com -o get-docker.sh 
+        sudo sh get-docker.sh
+        sudo usermod -aG docker \$USER
+    fi
 
     # 安装Docker Compose
-    command -v docker-compose &> /dev/null || {
-        echo "Installing Docker Compose...";
-        curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        chmod +x /usr/local/bin/docker-compose;
-    }
-    command -v docker-compose &> /dev/null && echo "Docker Compose已安装" || {
-        echo "Docker Compose安装失败。";
-        exit 1; 
-    }
+    if ! command -v docker-compose &> /dev/null; then
+        sudo curl -L \"https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)\" -o \"/usr/local/bin/docker-compose\"
+        sudo chmod +x \"/usr/local/bin/docker-compose\"
+    fi
+
+    echo \"安装完成\"
+
+    # 结束当前的bash会话，此时tmux会话也会自动结束
+    exit
+    " C-m
 }
 
 check_network() {
@@ -52,18 +74,23 @@ check_network() {
     ping -c 1 8.8.8.8 &> /dev/null || { echo "网络连接不可用。"; exit 1; }
 }
 
+
+
 get_public_ip() {
-    # 获取公网IP
     ip_services=("ifconfig.me" "ipinfo.io/ip" "icanhazip.com" "ipecho.net/plain" "ident.me")
     public_ip=""
     for service in "${ip_services[@]}"; do
-        public_ip=$(curl -s "$service")
-        if [[ "$public_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-            echo "公网IP: $public_ip"
-            break
+        if public_ip=$(curl -s "$service" 2>/dev/null); then
+            if [[ "$public_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+                echo "公网IP: $public_ip"
+                break
+            else
+                echo "$service 返回的不是一个有效的IP地址：$public_ip"
+            fi
         else
-            echo "$service 无法获取公网IP"
+            echo "$service 无法连接或响应太慢"
         fi
+        sleep 1  # 在尝试下一个服务之前稍微延迟
     done
     [[ "$public_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] || { echo "所有服务都无法获取公网IP。"; exit 1; }
 }
@@ -72,7 +99,7 @@ get_location() {
     # 获取主机位置
     location_services=("http://ip-api.com/line?fields=city" "ipinfo.io/city" "https://ip-api.io/json | jq -r .city")
     for service in "${location_services[@]}"; do
-        LOCATION=$(curl -s "$service")
+        LOCATION=$(curl -s "$service" 2>/dev/null)
         if [ -n "$LOCATION" ]; then
             echo "主机位置：$LOCATION"
             break
@@ -83,39 +110,68 @@ get_location() {
     done
     [ -n "$LOCATION" ] || echo "无法获取城市名。"
 }
-function setup_environment {
 
-   # Set DNS servers
-    echo -e "nameserver 8.8.4.4\nnameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 
-   # Install tmux, mosh, iptables, and netfilter-persistent
-   sudo apt update && sudo apt install -y tmux mosh iptables netfilter-persistent
- 
-   # Open UDP port range 60000 to 61000
-   sudo iptables -A INPUT -p udp --dport 60000:61000 -j ACCEPT
+# Your function
+setup_environment() {
 
-   # Save iptables rules using netfilter-persistent
-   sudo iptables-save > /etc/iptables/rules.v4
-   sudo service netfilter-persistent reload
 
-   # Update packages and dependencies
-   sudo apt upgrade -y
+# Set DNS servers
+echo -e "nameserver 8.8.4.4\nnameserver 8.8.8.8" > /etc/resolv.conf
+echo "DNS servers updated successfully."
 
-  # apt dist-upgrade -y && apt full-upgrade -y
-  [ -f "/proc/sys/net/ipv4/tcp_fastopen" ] && echo 3 | sudo tee /proc/sys/net/ipv4/tcp_fastopen
-  docker system prune -af --volumes
-  sudo iptables -A INPUT -p tcp --tcp-flags SYN SYN -j ACCEPT
-  curl -o Linux.sh https://raw.githubusercontent.com/EAlyce/ToolboxScripts/master/Linux.sh && chmod +x Linux.sh && ./Linux.sh
+# Install necessary packages (non-interactive mode)
+export DEBIAN_FRONTEND=noninteractive
+apt-get update > /dev/null
+apt-get install -y iptables netfilter-persistent > /dev/null
+echo "Necessary packages installed."
+
+# Open UDP port range and save iptables rules using netfilter-persistent
+iptables -A INPUT -p udp --dport 60000:61000 -j ACCEPT > /dev/null
+echo "UDP port range opened."
+iptables-save > /etc/iptables/rules.v4
+service netfilter-persistent reload > /dev/null
+echo "Iptables saved."
+
+# Update packages and dependencies (non-interactive mode)
+apt-get upgrade -y > /dev/null
+echo "Packages updated."
+
+# Enable TCP fast open if supported
+if [ -f "/proc/sys/net/ipv4/tcp_fastopen" ]; then
+  echo 3 > /proc/sys/net/ipv4/tcp_fastopen > /dev/null
+  echo "TCP fast open enabled."
+fi
+
+# Docker system prune
+docker system prune -af --volumes > /dev/null
+echo "Docker system pruned."
+
+# Additional configurations
+iptables -A INPUT -p tcp --tcp-flags SYN SYN -j ACCEPT > /dev/null
+echo "SYN packets accepted."
+
+  # 设置脚本名和tmux会话名
+  script_name="Linux.sh"
+  session_name="${script_name%.*}"  # 去掉文件扩展名
+
+  # 创建新的tmux会话
+  tmux new-session -d -s "$session_name"
+
+  # 在tmux中执行以下命令下载、授权并运行脚本
+  tmux send-keys -t "$session_name" "curl -o $script_name https://raw.githubusercontent.com/EAlyce/ToolboxScripts/master/$script_name && chmod +x $script_name && ./$script_name && echo '优化完成' && exit" C-m
+
+  # 关闭tmux会话（不等待）
+  tmux kill-session -t "$session_name"
 }
-function select_version {
+
+select_version() {
   echo "请选择 Snell 的版本："
   echo "1. v3"
   echo "2. v4 Surge专属"
   read -p "输入选择（默认选择2）: " choice
 
-  if [[ "$choice" != "1" ]]; then
-    choice="2"
-  fi
+  choice="${choice:-2}"
 
   case $choice in
     1) BASE_URL="https://github.com/xOS/Others/raw/master/snell"; SUB_PATH="v3.0.1/snell-server-v3.0.1"; VERSION_NUMBER="3" ;;
@@ -124,38 +180,46 @@ function select_version {
   esac
 }
 
-function select_architecture {
+select_architecture() {
   ARCH="$(uname -m)"
-  [[ "$ARCH" == "aarch64" ]] && ARCH_TYPE="linux-aarch64.zip" || ARCH_TYPE="linux-amd64.zip"
+  ARCH_TYPE="linux-amd64.zip"
+
+  if [ "$ARCH" == "aarch64" ]; then
+    ARCH_TYPE="linux-aarch64.zip"
+  fi
+
   SNELL_URL="${BASE_URL}/${SUB_PATH}-${ARCH_TYPE}"
 }
 
-function generate_port {
+generate_port() {
   EXCLUDED_PORTS=(5432 5554 5800 5900 6379 8080 9996)
 
-  PORT_NUMBER=$(shuf -i 5000-9999 -n 1)
-
-  while nc -z 127.0.0.1 $PORT_NUMBER || [[ " ${EXCLUDED_PORTS[@]} " =~ " ${PORT_NUMBER} " ]]; do
+  while true; do
     PORT_NUMBER=$(shuf -i 5000-9999 -n 1)
+
+    if ! nc -z 127.0.0.1 "$PORT_NUMBER" && [[ ! " ${EXCLUDED_PORTS[@]} " =~ " ${PORT_NUMBER} " ]]; then
+      break
+    fi
   done
 }
 
-function setup_firewall {
-  sudo iptables -A INPUT -p tcp --dport $PORT_NUMBER -j ACCEPT
-  echo "端口 $PORT_NUMBER "
+setup_firewall() {
+  sudo iptables -A INPUT -p tcp --dport "$PORT_NUMBER" -j ACCEPT || { echo "Error: Unable to add firewall rule"; exit 1; }
+  echo "防火墙规则已添加，允许端口 $PORT_NUMBER 的流量"
 }
 
-function generate_password {
-  PASSWORD=$(openssl rand -base64 12)
-  echo "密码：$PASSWORD"
+generate_password() {
+  PASSWORD=$(openssl rand -base64 12) || { echo "Error: Unable to generate password"; exit 1; }
+  echo "密码已生成：$PASSWORD"
 }
 
-function setup_docker {
+setup_docker() {
   NODE_DIR="/root/snelldocker/Snell$PORT_NUMBER"
-  mkdir -p $NODE_DIR
-  cd $NODE_DIR
+  
+  mkdir -p "$NODE_DIR" || { echo "Error: Unable to create directory $NODE_DIR"; exit 1; }
+  cd "$NODE_DIR" || { echo "Error: Unable to change directory to $NODE_DIR"; exit 1; }
 
-  cat > ./docker-compose.yml << EOF
+  cat <<EOF > docker-compose.yml
 version: "3.8"
 services:
   snell:
@@ -169,8 +233,8 @@ services:
       - ./snell-conf/snell.conf:/etc/snell-server.conf
 EOF
 
-  mkdir -p ./snell-conf
-  cat > ./snell-conf/snell.conf << EOF
+  mkdir -p ./snell-conf || { echo "Error: Unable to create directory $NODE_DIR/snell-conf"; exit 1; }
+  cat <<EOF > ./snell-conf/snell.conf
 [snell-server]
 listen = 0.0.0.0:$PORT_NUMBER
 psk = $PASSWORD
@@ -179,14 +243,15 @@ obfs = off
 ipv6 = false
 EOF
 
-  docker-compose up -d
-}
+  docker-compose up -d || { echo "Error: Unable to start Docker container"; exit 1; }
 
-function print_node {
+  echo "节点设置完成，以下是你的节点信息"
+}
+print_node() {
   if [ "$choice" == "1" ]; then
     echo "  - name: $LOCATION Snell v$VERSION_NUMBER $PORT_NUMBER"
     echo "    type: snell"
-    echo "    server: $(curl -s ifconfig.me)"
+    echo "    server: $public_ip"
     echo "    port: $PORT_NUMBER"
     echo "    psk: $PASSWORD"
     echo "    version: $VERSION_NUMBER"
@@ -198,8 +263,10 @@ function print_node {
   fi
 }
 
+
 main(){
 check_root
+set_custom_path
 install_tools
 clean_lock_files
 install_docker_and_compose
@@ -215,4 +282,5 @@ generate_password
 setup_docker
 print_node
 }
+
 main
