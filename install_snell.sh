d45485e94641990d57fa3f758bba7104309fc72c
Alas <48439969+EAlyce@users.noreply.github.com>
2024-01-02 22:05:11 +0800

    Update

--- install_snell.sh
+++ install_snell.sh
@@ -234,7 +234,7 @@ setup_docker() {
   cd "$NODE_DIR" || { echo "Error: Unable to change directory to $NODE_DIR"; exit 1; }
 
   cat <<EOF > docker-compose.yml
-version: "3.8"
+version: "3.3"
 services:
   snell:
     image: accors/snell:latest
