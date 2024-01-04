1d3bede10291aa2dc21f901e0a3a7f83fd6d2447
Alas <48439969+EAlyce@users.noreply.github.com>
2024-01-05 06:57:36 +0800

    更新 install_snell.sh

--- install_snell.sh
+++ install_snell.sh
@@ -252,7 +252,7 @@ python3 -m venv "$venv_dir/$venv_name"
 source "$venv_dir/$venv_name/bin/activate"
 }
 main(){
-create_and_activate_venv
+#create_and_activate_venv
 select_version
 clean_lock_files
 install_tools
@@ -267,7 +267,7 @@ generate_password
 setup_docker
 print_node
 # 退出虚拟环境
-deactivate
+#deactivate
 }
 
 main
